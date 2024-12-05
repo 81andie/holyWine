922041821efa50e0d49ed9c6bf6de04a0af12798
81andie <133395577+81andie@users.noreply.github.com>
2024-12-05 16:02:38 +0100

    guardando copia de las imagene a partir de l apisearchcustom de google

--- src/app/services/wine-card-service.ts
+++ src/app/services/wine-card-service.ts
@@ -15,7 +15,7 @@ export class WineCardService {
   private apiUrl= "https://gist.githubusercontent.com/ajubin/d331f3251db4bd239c7a1efd0af54e38/raw/058e1ad07398fc62ab7f3fcc13ef1007a48d01d7/wine-data-set.json";
 
   private CX = '15bb7538872eb471b'; // Tu Search Engine ID
-  private apiKey = 'AIzaSyB0-YiiUNHYu_BLPjXnTEtOvM5Dxrva1FI';
+  private apiKey = 'AIzaSyAbyGM6dh5oRbtS9tGVCqVDFiinGpsBP8o';
 
   getAllBottles():Observable<Wine[]>{
     return this.http.get<Wine[]>(this.apiUrl)
@@ -24,16 +24,6 @@ export class WineCardService {
     )
   }
 
-  fetchImageFromGoogle(query: string, numImages: number = 1): Observable<string[]> {
-    const url = `https://www.googleapis.com/customsearch/v1?q=${query}&cx=${this.CX}&key=${this.apiKey}&searchType=image&num=${numImages}`;
-    return this.http.get<any>(url).pipe(
-      map((data) => {
-        if (data.items && data.items.length > 0) {
-          return data.items.map((item: any) => item.link); // Devuelve las URLs de las imágenes
-        }
-        return [];
-      })
-    );
-  }
+
 
 }
