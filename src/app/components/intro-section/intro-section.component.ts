3d26da9a9748474a4cd56ea7864c2499a2cf83fc
81andie <133395577+81andie@users.noreply.github.com>
2024-12-10 21:33:39 +0100

    Acabando de hacer animaciones en la cabecera

--- src/app/components/intro-section/intro-section.component.ts
+++ src/app/components/intro-section/intro-section.component.ts
@@ -1,9 +1,12 @@
+
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { SearchBarComponent } from "../search-bar/search-bar.component";
 
 @Component({
   selector: 'app-intro-section',
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatProgressBarModule, SearchBarComponent],
   templateUrl: './intro-section.component.html',
   styleUrl: './intro-section.component.css'
 })
