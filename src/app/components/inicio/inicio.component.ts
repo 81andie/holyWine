b3b5af0acac6ed33316f72bfe1b2d039a01a32fc
81andie <133395577+81andie@users.noreply.github.com>
2024-12-14 21:56:09 +0100

    quitando algunos imports que no utilizo y he cambiado el bg-color de las cards

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { SidenavComponent } from '../sidenav/sidenav.component';
 import { IntroSectionComponent } from "../intro-section/intro-section.component";
-import { SearchBarComponent } from "../search-bar/search-bar.component";
-import { CardsComponent } from '../cards/cards.component';
+
 
 
 
 @Component({
   selector: 'app-inicio',
-  imports: [SidenavComponent, IntroSectionComponent, SearchBarComponent, CardsComponent],
+  imports: [ IntroSectionComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
