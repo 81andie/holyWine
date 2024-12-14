b5dcbbd1d1dabe33584689871844f314afc3a4e1
81andie <133395577+81andie@users.noreply.github.com>
2024-12-14 23:08:59 +0100

    Sacando las variedades

--- src/app/components/variety-grapes/variety-grapes.component.ts
+++ src/app/components/variety-grapes/variety-grapes.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Wine } from '../../interfaces/winecard.interface';
+import { WineCardService } from '../../services/wine-card-service';
 
 @Component({
   selector: 'app-variety-grapes',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './variety-grapes.component.html',
   styleUrl: './variety-grapes.component.css'
 })
-export class VarietyGrapesComponent {
+export class VarietyGrapesComponent implements OnInit {
+
+  constructor(private wineService: WineCardService) { }
+
+  ngOnInit(): void {
+    this.searchVarietyWines();
+
+  }
+
+  private wine: Wine[] = [];
+  private wineVariety: string[] = [];
+  private wineVarietyUnique: string[] = [];
+
+  searchVarietyWines() {
+    this.wineService.getAllBottles1().subscribe(data => {
+      this.wine = data;
+
+      let multipleVariety = this.wine.map((variety: { variety: string }) =>(variety.variety))
+      console.log(multipleVariety)
+
+      let unique = new Set(multipleVariety.flat());
+      this.wineVarietyUnique = [...unique];
+      console.log(this.wineVarietyUnique)
+
+      })
+
+
+  }
+
 
 }
+
+
