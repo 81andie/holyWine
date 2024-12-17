2c5e787b98dca104ccfc1b70c0595dcca1408feb
81andie <133395577+81andie@users.noreply.github.com>
2024-12-17 23:24:28 +0100

    Acabando el footer y retocando los borders y  linear gradients de la seccion intro-section1

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, RouterModule, MatButtonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
