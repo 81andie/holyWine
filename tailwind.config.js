ddb4ca238f6a8a71246b9e92fb2c708fa6e1d018
81andie <133395577+81andie@users.noreply.github.com>
2024-12-20 22:55:40 +0100

    Intento de cambiar la paleta de colores de la web, pero no me convence, lo desago

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    colors: {
-      'burgundy': '#800020',
-      'wineRed': '#58181F',
-      'BurgundyWine': '#6C403E',
-      'pantone': '#8c796e;',
-      'blackberryBurgundi': '#4C3938',
-      'bruised': '#5b4148'
-
-    },
+  
     extend: {},
   },
   plugins: [],
