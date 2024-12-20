524fa67f6d6ecdf12c7509eb28c857d9aaace1e4
81andie <133395577+81andie@users.noreply.github.com>
2024-12-20 22:53:28 +0100

    cambiando la paleta de colores(prueba)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    colors: {
+      'burgundy': '#800020',
+      'wineRed': '#58181F',
+      'BurgundyWine': '#6C403E',
+      'pantone': '#8c796e;',
+      'blackberryBurgundi': '#4C3938',
+      'bruised': '#5b4148'
+
+    },
     extend: {},
   },
   plugins: [],
