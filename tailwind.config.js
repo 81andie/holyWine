cba36ed9b15954e9962c6c59329e2753dff7598d
81andie <133395577+81andie@users.noreply.github.com>
2024-12-21 19:38:21 +0100

    Generando una nueva paleta de colores añadiendola al sistema de colores de tailwind, y pasandola a todos los componentes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-  
-    extend: {},
+    extend: {
+      colors: {
+        pantone: {
+          '17-tendril': 'var(--pantone-17-mochamousse)',
+          '17-tendril': 'var(--pantone-17-tendril)',
+          '17-cornflower-blue': 'var(--pantone-17-cornflowerblue)',
+          '17-viola': 'var(--pantone-17-viola)',
+          '17-rosetan': 'var(--pantone-17-rosetan)',
+          '17-cobblestone': 'var(--pantone-17-cobblestone)',
+          '17-mocha-willow': 'var(--pantone-17-mochawillow)',
+          '17-gardenia': 'var(--pantone-17-gardenia)',
+        },
+      },
+    },
+
   },
   plugins: [],
 }
