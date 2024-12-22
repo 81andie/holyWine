774a1620b116aefb988074433a27db8c99273c41
81andie <133395577+81andie@users.noreply.github.com>
2024-12-22 16:07:44 +0100

    preparar la aplicación para subirla a  gh-pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import InicioComponent from './components/inicio/inicio.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { CatalogComponent } from './components/catalog/catalog.component';
 import { VarietyGrapesComponent } from './components/variety-grapes/variety-grapes.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 
 export const routes: Routes = [
   { path: 'inicio', component: InicioComponent },
@@ -10,6 +11,7 @@ export const routes: Routes = [
   { path: 'variedad', component: VarietyGrapesComponent },
   { path: 'contacto', component: ContactoComponent },
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: '**', component: ErrorPageComponent }
 
 
 
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,11 +3,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule, RouterModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, RouterModule,RouterLink, MatButtonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
